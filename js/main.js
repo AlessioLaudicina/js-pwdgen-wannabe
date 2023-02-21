e74f01b44b49a1baee49c4c6a50e0ec6c989e6f2
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-02-21 15:27:55 +0100

    prompt

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-prompt("Come ti chiami?");
+let myName = prompt("Come ti chiami?");
 
-prompt("E il tuo cognome?");
 
-prompt("Quale è il tuo colore preferito?");
+let mySur = prompt("E il tuo cognome?");
+
+let myColor = prompt("Quale è il tuo colore preferito?");
+
+document.getElementById('hello').innerHTML = myName + mySur + myColor + "23"
