8713b0f66bf80ee7587dd8a3f4863f84d326bb4b
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-02-22 09:49:27 +0100

    correzioni

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-let myName = prompt("Come ti chiami?");
 
 
-let mySur = prompt("E il tuo cognome?");
-
-let myColor = prompt("Quale è il tuo colore preferito?");
+const myName = prompt("Come ti chiami?");
+const mySur = prompt("E il tuo cognome?");
+const myColor = prompt("Quale è il tuo colore preferito?");
 
 document.getElementById('hello').innerHTML = myName + mySur + myColor + "23"
